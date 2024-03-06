eef252a3a5992a4367260c8ae36d3216cd8cf511
ALXSAI <alexandersainishvili@gmail.com>
2024-03-07 01:31:05 +0400

    changed i and j indexes

--- tictactoe.py
+++ tictactoe.py
@@ -45,7 +45,7 @@ def actions(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == EMPTY:
-                res.append((i,j))
+                res.append((j,i))
     return set(res)
 
 
